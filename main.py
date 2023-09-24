2f0aae4581f9dbb08d022fd41c3da0546d3e2391
sergiu <sergiudubalari112@gmail.com>
2023-09-24 11:11:51 +0300

    main

---
+++ main.py
@@ -0,0 +1,71 @@
+
+#array =["GENERAL", "234", "MATH", "4567", "INFO",]
+#int_array=[1 ,2,3,9,8,9,3]
+#float_array=[1.4,6.7 , 7.9 -90,2.0,5,6]
+#bool_array=[True, False, True,False]
+
+#array.append("English")  #append is  for adding smath
+#array = sorted(array) # is used for sorting
+#array.sort()
+#int_array.sort(reverse=True) # soart ( revarse=True
+#print(int_array)  #print list
+#print(sorted(int_array))
+
+
+
+
+
+#list = []
+#list_length = int(input("Enter list Lenght"))
+
+#for i in range (0, list_lenght):
+    #element = int (input ("Enter Element"))
+    #list.append(element)
+#else:
+   # print(list)
+    #print(list.sort())
+   # print(list.sort(revarse=True)
+        #  print(list)
+
+
+#list = ["12" , "2568", "56", "345", "456", "175"]
+#print(list[3])
+
+#if "56" in list:
+    #print(\True)
+#else:
+    #print(False)
+
+
+
+
+   # list = ["12", "2568", "56", "345", "456", "175"]
+    #print(list[3])
+    #for i in range (0, len (list)):
+        #print(list[i]) # print list by iandex
+
+       # for e in list:
+           # print(e) print by element
+
+
+          # tuple = ("A", "B", "C", "D")
+
+          # print(tuple + ( "E", "Finish"))
+
+#int_array = [1,2,3,4,5,6,7,]
+#int_array =  set (int_array)
+#set_array = {1,1,2,3,1,,5,2,6,7,}
+#print(set_array)
+#print(set_array)
+
+
+dex = {"COD": "Python", "Extend": "py", "Less": "3"}
+
+print(dex.get("Cod")) # use key to display value with
+
+dex.update({"Less": 3}) #update value where key is Less
+
+for i in dex :
+    print(i) #display key
+    print(dex.get(i)) # display value with key (i)
+
